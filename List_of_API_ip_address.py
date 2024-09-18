4f15aee985c615e056b78a7a103666fef1eb12f7
Chanapai Chuadchum <kornbot359@hotmail.com>
2024-09-18 09:19:34 +0700

    Add files via upload

---
+++ List_of_API_ip_address.py
@@ -0,0 +1,9 @@
+database = "http://192.168.50.247:5690/total_database"
+generated_token = "http://192.168.50.247:4887/get_token_store"
+get_devices_info_client = "https://roboreactor.com/get_device_info"
+total_components_selected = "http://192.168.50.247:4678/get_total_project"
+generated_project_code_features = "http://192.168.50.247:3689/total_classrestore"
+payload_to_generate_code = "http://192.168.50.247:3689/total_codegenerated"
+current_project_selected = "http://192.168.50.247:4678/current_selected_componentsmap"
+document_match_component_selection ="http://192.168.50.247:7884/doccompmatch_total"
+
